4dd3438e453efa4b70657803d2aff42ce00f2147
Ashkan <ashkan.yaghmorlou@gmail.com>
2021-02-23 14:37:04 -0500

    Completed front-end Routing

--- app/Main.js
+++ app/Main.js
@@ -1,18 +1,30 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter, Switch, Route } from "react-router-dom"
 
 // My Compoenents
 import Header from "./components/Header"
 import HomeGeust from "./components/HomeGuest"
 import Footer from "./components/Footer"
-
+import About from "./components/About"
+import Terms from "./components/Terms"
 function Main() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGeust />
+      <Switch>
+        <Route path="/" exact>
+          <HomeGeust />
+        </Route>
+        <Route path="/about-us">
+          <About />
+        </Route>
+        <Route path="/terms">
+          <Terms />
+        </Route>
+      </Switch>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
